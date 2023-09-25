97c00f1e50528e89a9230a86826be97b8bcd76b1
JoelDiazQuinto <joeldiazquinto13@gmail.com>
2023-09-24 23:18:00 -0600

    Cambio en css del login y el login mismo

--- src/pages/ComponenteLogin.jsx
+++ src/pages/ComponenteLogin.jsx
@@ -3,16 +3,61 @@ import styles from '../css/ComponenteLogin.css';
 import { Link } from 'react-router-dom';
 
 export default function login() {
-    
-    return (
-        <div id="principal">
-            <div id="login">
-                <div id="google">
-                    <p class="texto">Iniciar sesion con google</p>
-                    <button id="botonlogin"></button>
 
+    return (
+
+
+
+
+
+        <div class="principal">
+
+            <div class="menu">
+                <p>Inicio de Sesión "El ñon"</p>
+                <div class="opc-menu">
+                    <p>Inicio</p>
+                    <p>Registro</p>
+                    <p>Carrito</p>
                 </div>
-                <div id="credenciales">
+            </div>
+
+            <div class="contenedor">
+                <div class="google">
+                    <p>Iniciar sesión con google</p>
+                    <img></img>
+                </div>
+
+                <hr width="0" size="500" color="black"></hr>
+
+                <div class="iniciar">
+                    <p>Login</p>
+                    <div class="datosIniciar">
+                        <p>Usuario</p>
+                        <input type="text" class="campo" placeholder="Ingresa Usuario"></input>
+                        <p>Contraseña</p>
+                        <input type="password" class="campo" placeholder="Ingresa Contraseña"></input>
+                    </div>
+                    <button>Entrar</button>
+                </div>
+            </div>
+
+        </div>
+
+
+
+
+
+
+
+        /*
+            <div id="principal">
+                <div id="login">
+                    <div id="google">
+                        <p class="texto">Iniciar sesion con google</p>
+                        <button id="botonlogin"></button>
+
+                    </div>
+                    <div id="credenciales">
                         <div>
                             <p id="textoGoogle" class="texto">LOGIN</p>
                         </div>
@@ -21,20 +66,20 @@ export default function login() {
                                 <label htmlFor='txtUsuario'>Usuario</label>
                                 <input type="text" id="txtUsuario"></input>
                             </div>
-                            
+
                             <div id="divPass">
                                 <label htmlFor='txtPass'>Contraseña</label>
                                 <input type="password" id="txtPass"></input>
                             </div>
-                            
+
                         </div>
                         <div>
                             <Link to="/"><button >Entrar</button></Link>
                         </div>
-                    
+
+                    </div>
                 </div>
-            </div>
-        </div>
+            </div>*/
     )
 }
 
