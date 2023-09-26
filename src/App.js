b71e8afc20a15be00cc3ae3b61b4d9b704a70088
deivis8 <deividsolis183@gmail.com>
2023-09-25 21:50:28 -0600

    menuPrincipal

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import logo from './logo.svg';
 import React from 'react';
 import './App.css';
 import ComponenteLogin from './pages/ComponenteLogin';
+import MenuPrincipal from './pages/MenuPrincipal';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path="/login" element={<ComponenteLogin />}>
-        
-      </Route>
+      <Route path="/login" element={<ComponenteLogin />}>   </Route>
+      <Route path="/MenuPrincipal" element={<MenuPrincipal />}>   </Route>
+      
     </Routes>
   </BrowserRouter>
   );
