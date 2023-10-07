41ab82df3b0d6374cea84df38d601aa99f0dc61a
Pedro R M <dark1473@gmail.com>
2023-10-07 16:41:35 -0600

    Test login

--- src/pages/pruebas.jsx
+++ src/pages/pruebas.jsx
@@ -1,7 +1,36 @@
 import React from 'react'
+function register() {
+  const data = {
+    name: 'John Doe',
+    email: 'user@example.com',
+    password: 'password123',
+  };
 
+  fetch('http://serverreyes.ddns.net:8000/register', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+    .then(response => response.json())
+    .then(data => {
+      // Maneja la respuesta del servidor aquí (por ejemplo, muestra un mensaje de éxito)
+      console.log('Registro exitoso', data);
+    })
+    .catch(error => {
+      // Maneja errores aquí (por ejemplo, muestra un mensaje de error)
+      console.error('Error en el registro', error);
+    });
+}
 export default function fsdfsdf() {
   return (
-    <div>dfdfdf</div>
+    <div>
+      {register()}
+
+
+    </div>
+
+
   )
 }
