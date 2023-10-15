3ba531c6a4e1ae1c2f260233c882413a62b22612
Pedro <dark1473@gmail.com>
2023-10-14 23:18:52 -0600

    El header muestra ,dependiendo de si esta autenticado o no, los botones "Login" ,y "registro"  o solamente "Logout"

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,13 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import Auth from './Auth';
 import styles from '../css/Header.css';
 import { Link } from 'react-router-dom';
 export default function Header(props) {
+    const [isAuthenticated, setIsAuthenticated] = useState(null); // Usar null para indicar "cargando"
+
+    useEffect(() => {
+      Auth()
+        .then((isAuthenticated) => {
+          setIsAuthenticated(isAuthenticated);
+        })
+        .catch((error) => {
+          console.log('Error en la solicitud:', error);
+          setIsAuthenticated(false);
+        });
+    }, []);
+
+    
+
     return (
         <div class="menu">
             <p>{props.Titulo}</p>
             <div class="opc-menu">
                 <Link to={"/"}><p>Inicio</p></Link>
-                <Link to={'/Registro'}><p>Registro</p></Link>
+                {isAuthenticated ? (
+                <Link to={'/Logout'}><p>Logout</p></Link> ) : (
+                <div><Link to={'/Registro'}><p>Registro</p></Link></div>
+                ) }
+                {isAuthenticated ? (<></>) : (
+                <div><Link to={'/Login'}><p>Login</p></Link></div>
+                ) }
                 <p>Carrito</p>
             </div>
         </div>
