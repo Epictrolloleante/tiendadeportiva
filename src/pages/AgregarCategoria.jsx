780848ce3dcabf8251abf16612e412d7c740b7ab
JoelDiazQuinto <joeldiazquinto13@gmail.com>
2023-11-07 11:51:05 -0600

    Correcciones 2

--- src/pages/AgregarCategoria.jsx
+++ src/pages/AgregarCategoria.jsx
@@ -1,102 +1,44 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
+
 import ComponenteLayout from './ComponenteLayout';
 import styles from '../css/AgregarCategoria.css';
 
 export default function AgregarCategoria() {
-  const [formData, setFormData] = useState({
-    nombre: '',
-    imagen: '', // Aquí guardaremos solo el nombre de la imagen
-    altimagen: '',
-  });
+    
+    useEffect(() => {
+        const file = document.getElementById('file-input');
+        const img = document.getElementById('imagen');
+        const defaultFile = 'https://ingeniosas.org/wp-content/plugins/bb-plugin/img/no-image.png';
+    
+        file.addEventListener('change', e => {
+          if (e.target.files[0]) {
+            const reader = new FileReader();
+            reader.onload = function (e) {
+              img.src = e.target.result;
+            };
+            reader.readAsDataURL(e.target.files[0]);
+          } else {
+            img.src = defaultFile;
+          }
+        });
+      }, []); // El segundo argumento vacío [] indica que se ejecutará una vez después del montaje
+    
 
-  const [imagen, setImagen] = useState('https://ingeniosas.org/wp-content/plugins/bb-plugin/img/no-image.png');
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
-
-  const handleFileChange = (e) => {
-    if (e.target.files[0]) {
-      // Extraer el nombre del archivo de la URL
-      const fileName = e.target.files[0].name;
-      setImagen(fileName);
-
-      // Actualizar formData para guardar solo el nombre del archivo
-      setFormData({
-        ...formData,
-        imagen: fileName,
-      });
-    } else {
-      setImagen('https://ingeniosas.org/wp-content/plugins/bb-plugin/img/no-image.png');
-    }
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    // Realiza la solicitud POST al servidor para agregar la categoría
-    fetch('http://serverreyes.ddns.net:8000/api/categories', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(formData),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        // Maneja la respuesta del servidor (por ejemplo, confirma que la categoría se agregó correctamente)
-        console.log('Respuesta del servidor:', data);
-      })
-      .catch((error) => {
-        // Maneja errores en la solicitud
-        console.error('Error en la solicitud:', error);
-      });
-  };
-
-  return (
-    <ComponenteLayout Titulo='Categoria'>
-      <div className='fondoP'>
-        <div className='contenedor'>
-          <div className='registro'>
-            <h3>REGISTRO DE CATEGORIAS</h3>
-            <form onSubmit={handleSubmit}>
-              <div className='infoR'>
-                <p>Categoria:</p>
-                <input
-                  type='text'
-                  placeholder='Introduce el nombre de la categoria'
-                  name='nombre'
-                  value={formData.nombre}
-                  onChange={handleChange}
-                />
-                <p>Alt Imagen:</p>
-                <input
-                  type='text'
-                  placeholder='Introduce el nombre de la imagen'
-                  name='altimagen'
-                  value={formData.altimagen}
-                  onChange={handleChange}
-                />
-                <label htmlFor='file-input' className='custom-file-upload'></label>
-                <input
-                  type='file'
-                  id='file-input'
-                  className='hidden-file-input'
-                  onChange={handleFileChange}
-                  accept='image/*' // Solo permite archivos de imagen
-                />
-                <img id='imagen' src={imagen} alt='Vista previa de la imagen' />
-              </div>
-              <button type='submit'>Registrar</button>
-            </form>
-          </div>
-        </div>
-      </div>
-    </ComponenteLayout>
-  );
+    return (
+        <ComponenteLayout Titulo='Categoria'>
+            <div className='fondoP'>
+                <div className='contenedor'>
+                    <div className='registro'>
+                        <h3>REGISTRO DE CATEGORIAS</h3>
+                        <div className='infoR'>
+                            <p>Categoria:</p> <input type='text' placeholder='Introduce el nombre de la categoria'></input> 
+                            <p>Alt Imagen:</p> <input type='text' placeholder='Introduce el nombre de la imagen'></input>
+                            <label for="file-input" class="custom-file-upload"></label> <input type="file" id="file-input" class="hidden-file-input" /> <img id='imagen' src='https://ingeniosas.org/wp-content/plugins/bb-plugin/img/no-image.png'></img>
+                        </div>
+                        <button>Registrar</button>
+                    </div>
+                </div>
+            </div>
+        </ComponenteLayout>
+    )
 }
